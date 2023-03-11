f62b5181785b4a15e053c4fc0d8e85fa21d1ee19
Ignacio Velazquez <ignaciov92@gmail.com>
2023-03-11 14:51:12 -0300

    nabvar y estructura del footer

--- tarea2/src/index.js
+++ tarea2/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar2 from './components/Navbar';
+import Footer from './components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar2 />
+    <Footer/>
   </React.StrictMode>
 );
 
